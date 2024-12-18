9d7c5734d9c0fff9e36de69217734bd3b431a31f
Junaid Amjad <junaid.amjad@tetralogicx.com>
2024-12-18 15:31:54 +0500

    localization

--- resources/lang/en/message.php
+++ resources/lang/en/message.php
@@ -5,5 +5,11 @@
     'Logout' => 'logout',
     'login' => 'login',
     'register' => 'register',
+    'Home' => 'Home ',
+    'Contact' =>'Contact',
+    'About_Us'=> "About Us",
+    'Service' => 'Service',
+    'Events' => 'Events',
+    'Dashboard' => 'Dashboard '
     // Add more translations as needed
 ];
